777e2d469806224dc778dc93f72f55a84ff76aa7
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-04-19 23:03:32 +0300

    Add assignment 1

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-	base: '/~ksenia/routing/',
+	base: '/~ksenishl/wsk-routing/',
 })
